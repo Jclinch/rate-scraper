7e2011e4e9e6234a5d624e01328a75d2361cccac
sunny <sunnyugwu2011@gmail.com>
2025-08-02 21:47:31 +0100

    service_role

--- scraper.js
+++ scraper.js
@@ -6,7 +6,8 @@ import { createClient } from "@supabase/supabase-js";
 // 🔹 Supabase setup
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  // process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE
 );
 
 // 🔹 Save vendor rate to Supabase
